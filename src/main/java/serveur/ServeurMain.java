fa0cece449d7301de1177b8ffca3ef1cd922176a
Kalreg <fabhebert@hotmail.com>
2016-06-07 13:52:55 +0200

    Ajout suport Maven

---
+++ src/main/java/serveur/ServeurMain.java
@@ -0,0 +1,13 @@
+package serveur;
+
+/**
+ * Created by Kalreg on 07/06/2016.
+ */
+public class ServeurMain {
+	public final static int PortParDefaut = 3000;
+	private static int port;
+
+	public static void main(String[] args) {
+		// write your code here
+	}
+}
